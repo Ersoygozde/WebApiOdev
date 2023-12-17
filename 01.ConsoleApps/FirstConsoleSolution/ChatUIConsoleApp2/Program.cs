2e337af5f4ede05f846615b853c363ee660bcefd
GOZDE ERSOY <GOZDE ERSOY@EGEERSOY>
2023-12-17 15:22:54 +0300

    Add project files.

---
+++ 01.ConsoleApps/FirstConsoleSolution/ChatUIConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.Design;
+
+namespace ChatUIConsoleApp2;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hi, I am Chat UI.");
+        Console.WriteLine("I want to know you better.");
+        Console.WriteLine("Please tell me your name?");
+        string name = Console.ReadLine(); // sari ya da mavi alt cizgi warning
+        Console.WriteLine("Nice to meet you" + name);
+        Console.WriteLine("Where are you from?");
+        string from = Console.ReadLine();
+        Console.WriteLine("Oww, that's great! I am from US");
+        Console.WriteLine("How old are you?");
+        string age = Console.ReadLine(); // kirmizi alt cizgi hata
+        Console.WriteLine("Great! Ok, your name is " + name + " you write me from " + from + " and you are " + age + " years old.");
+        Console.WriteLine("Am I correct?");
+        string result = Console.ReadLine();
+        //sart bloglari
+        if (result == "Yes")
+        {
+            Console.WriteLine("That is great. I am really happy!");
+        }
+        else if (result == "No")
+        {
+            Console.WriteLine("Oww, I am sorry for misunderstanding");
+        }   
+        else 
+        {
+            Console.WriteLine("I don't understand your response!");
+        }
+        { 
+        
+
+
+
+        }
+    }       
+}
---
+++ 01.ConsoleApps/FirstConsoleSolution/MyFirstConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace MyFirstConsoleApp;
+
+internal class Program
+{// blok code blogu 
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        //string int decimal double float boolean datetime object
+        // type degisken_adi = degeri; // degisken syntax yapisi // syntax hatasi => kodlama yaparken kodda yapilan harf ya da tanimlama  hatasi
+        string nameLastname = "Taner Saydam";
+        int age = 33;
+        decimal salary = 47500.20m;
+        bool isActive = true; //false olumsuz -- 0 => false / 1 => true
+        DateTime startDate = DateTime; // September 26, 2023 21:23.10,000
+        string name = "Taner";
+        Console.WriteLine("Merhaba " + name);
+        string readLine = Console.ReadLine();
+        Console.WriteLine("Yazdigin sey: " + readLine);
+        string readline = Console.ReadLine();
+        Console.WriteLine("Yazdigin sey:" + readline);
+
+    }
+
+}// scope
