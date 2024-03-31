19c7ad4750f104d3dce445dd04321212ba62f11d
Ersoygozde <ersoygozde35@gmail.com>
2024-03-31 13:22:14 +0300

    change

---
+++ WebApiOdev/WebApiOdev/Controllers/CustomerController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiOdev.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class CustomerController : ControllerBase
+{
+}
